279d50aca24b64a77aacd83b42c43ed3b7f9732b
Kira Kopcho <kirak5683@gmail.com>
2024-03-16 13:20:18 -0700

    Added framework for selection of front mount selection (Arm, Mining)

---
+++ software/react_groundstation/src/components/ArmControl.js
@@ -0,0 +1,6 @@
+function ArmControl(){
+    return(
+        <h1> This is the arm UI</h1>
+    );
+}
+export default ArmControl;
---
+++ software/react_groundstation/src/components/MiningControl.js
@@ -0,0 +1,6 @@
+function MiningControl(){
+    return(
+        <h1> This is the Mining UI</h1>
+    );
+}
+export default MiningControl
