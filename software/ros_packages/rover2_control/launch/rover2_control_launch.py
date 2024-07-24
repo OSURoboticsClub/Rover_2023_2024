3a0cc4bc08fe1429defe59a7aad077693b6cbbb2
Blake Hakkila <hakkilab@oregonstate.edu>
2024-07-23 19:48:55 -0700

    fixed tower pan tilt node name

--- software/ros_packages/rover2_control/launch/rover2_control_launch.py
+++ software/ros_packages/rover2_control/launch/rover2_control_launch.py
@@ -74,7 +74,7 @@ def generate_launch_description():
         Node(
             package='rover2_control',
             executable='tower_and_pan_tilt_control',
-            name='tower_and_pan_tilt',
+            name='tower_pan_tilt',
             **config
         ),
         Node(
