0fde456d51a9512df1cf7f140f1249aefeca936e
Blake Hakkila <hakkilab@oregonstate.edu>
2024-07-28 01:52:27 +0000

    redid systemd startup of rover

--- software/startRover.sh
+++ software/startRover.sh
@@ -1,5 +1,6 @@
-xhost +
 sudo modprobe can
 sudo modprobe can_raw
 sudo modprobe mttcan
+sudo ip link set can0 down
 sudo ip link set can0 up type can bitrate 500000 dbitrate 1000000 berr-reporting on fd on
+sudo docker start rover2
