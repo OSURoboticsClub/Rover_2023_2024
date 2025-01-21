36f7f21becc8ffa0f2dd13862ad6998b7b9884cd
Henry Dalrymple <henrydalrymple53@gmail.com>
2025-01-21 15:38:42 -0800

    can shenanigans

--- software/startRover.sh
+++ software/startRover.sh
@@ -1,5 +1,5 @@
 sudo modprobe can
 sudo modprobe can_raw
 sudo modprobe mttcan
-sudo ip link set can0 down
-sudo ip link set can0 up type can bitrate 500000 dbitrate 1000000 berr-reporting on fd on
+sudo ip link set can1 down
+sudo ip link set can1 up type can bitrate 500000 dbitrate 1000000 berr-reporting on fd on
--- software/startRoverOrin.sh
+++ software/startRoverOrin.sh
@@ -1,3 +1,8 @@
+sudo modprobe can
+sudo modprobe can_raw
+sudo modprobe mttcan
+sudo ip link set can1 down
+sudo ip link set can1 up
 
 source /opt/ros/humble/setup.bash
 /home/makemorerobot/Rover_2023_2024/software/install/setup.bash
