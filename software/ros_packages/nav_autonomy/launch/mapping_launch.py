568d0bbbe2f29beed19d02579a56b2b8ad396dc9
Kylan <kyrileyj@gmail.com>
2025-11-21 16:28:14 -0800

    added some params and comments

--- software/ros_packages/nav_autonomy/launch/mapping_launch.py
+++ software/ros_packages/nav_autonomy/launch/mapping_launch.py
@@ -17,7 +17,7 @@ import os
         # odom -> base_link
         # base_link -> ... -> <camera_name>_color_optical_frame
 
-
+# KRJ TODO: convert this to a rtab param config file instead
 def generate_launch_description():
     parameters=[{
         "use_sim_time": True,
@@ -32,7 +32,9 @@ def generate_launch_description():
         'approx_sync':False,              # Set to false because images are all from single camera, so will be synced
         'publish_tf':False,
 
-        # Internal parameters (must be strings)
+# Internal parameters (must be strings)
+    # /Grid
+        # Configure generation of obstacle map
         'Grid/3D':"false",                 # We do not want octomap. Saves memory and time 
         'Grid/CellSize':".05",
         'Grid/MaxGroundAngle':"60",
@@ -43,9 +45,27 @@ def generate_launch_description():
         # 'Grid/NoiseFilteringRadius':".05",
         # 'Grid/NoiseFilteringMinNeighbors':"5",
 
-        # Fuse GPS as a prior
-        "Optimizer/PriorsIgnored":'false',
-        "Rtabmap/LoopGPS":'true'
+    # /Optimzer
+        "Optimizer/PriorsIgnored":'false',  # Fuse GPS as a prior
+            # Note from the docs: "Currently only g2o and gtsam optimization supports this."
+            # It is ambiguous whether this is refering to supporting priors or supporting ignoring priors
+            # By default the optimization method is neither of these, should be TORO (double check this)
+
+    # /Rtabmap
+            # Some general params for loop closure thresholds
+        "Rtabmap/LoopGPS":'true',
+
+
+        # Internal parameters (must be strings)
+        'Grid/3D':"false",                 # We do not want octomap. Saves memory and time
+        'Grid/RayTracing': 'true',         # Fill empty space
+
+    # /Reg and /Vis
+        # Configure point cloud registration (first step in sticking frames together)
+        
+    # /RGBD
+        # Configure loop closure and graph optimization for RGBD
+
         }]        
 
     remappings=[
