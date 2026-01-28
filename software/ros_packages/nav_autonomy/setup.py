9c6a9fa08e6129cb20d5bdc187a113f40251fcb9
Kylan <kyrileyj@gmail.com>
2026-01-27 17:02:49 -0800

    add yolo_server template

--- software/ros_packages/nav_autonomy/setup.py
+++ software/ros_packages/nav_autonomy/setup.py
@@ -32,6 +32,7 @@ setup(
             'mission_manager = nav_autonomy.mission_manager:main',
             'gps_nav2_tester = nav_autonomy.gps_nav2_tester:main',
             'mission_manager_test_client = nav_autonomy.mission_manager_test_client:main',
+            'yolo_server = nav_autonomy.yolo_server:main',
         ],
     },
 )
