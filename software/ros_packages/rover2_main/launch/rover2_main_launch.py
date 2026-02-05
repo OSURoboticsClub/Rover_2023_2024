3da97df2774136c78a2aca6e93a33ff4b18c3bbe
0xrd1 <nospam@email.com>
2026-02-05 15:45:21 -0800

    costmap changes raw point clouds

--- software/ros_packages/rover2_main/launch/rover2_main_launch.py
+++ software/ros_packages/rover2_main/launch/rover2_main_launch.py
@@ -4,6 +4,8 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+from launch.actions import SetEnvironmentVariable
+LOG_LEVEL = 'INFO' # DEBUG, INFO, WARN, ERROR, FATAL
 
 def generate_launch_description():
    drive_control = IncludeLaunchDescription(
@@ -42,6 +44,7 @@ def generate_launch_description():
          'launch'), '/nav_launch.py'])
       )
    return LaunchDescription([
+      SetEnvironmentVariable('RCUTILS_LOGGING_SEVERITY_THRESHOLD', LOG_LEVEL),
       drive_control,
       imu,
       arm,
@@ -49,5 +52,4 @@ def generate_launch_description():
       mapping,
       nav_autonomy,
       cameras,
-
    ])
