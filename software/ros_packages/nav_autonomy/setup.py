1d7f7cf4e5adacca9fc62d691039f877dea68d90
Kylan <kyrileyj@gmail.com>
2026-02-14 14:08:42 -0800

    rtab consumes global pose to align map heading

--- software/ros_packages/nav_autonomy/setup.py
+++ software/ros_packages/nav_autonomy/setup.py
@@ -33,6 +33,7 @@ setup(
             'gps_nav2_tester = nav_autonomy.gps_nav2_tester:main',
             'mission_manager_test_client = nav_autonomy.mission_manager_test_client:main',
             'yolo_server = nav_autonomy.yolo_server:main',
+            'map_pose_publisher = nav_autonomy.map_pose_publisher:main',
         ],
     },
 )
