ef964f8cd8a66eaf6fe8de37085c1ead60f083af
Henry Dalrymple <dalrymhe@oregonstate.edu>
2026-03-31 21:06:30 -0700

    march 31 leftover code from spectrometry integration test

--- software/ros_packages/rover2_spectrometry/rover2_spectrometry/spectrometry_publisher.py
+++ software/ros_packages/rover2_spectrometry/rover2_spectrometry/spectrometry_publisher.py
@@ -102,7 +102,7 @@ class Spectrometry_Publisher(Node):
             plt.clf()
 
     def get_graph(self, feedNumber, reaction_type):
-        cap = cv2.VideoCapture(feedNumber)
+        cap = cv2.VideoCapture("/dev/rover/camera_infrared")
         ret, frame = cap.read()
         cropped = frame[int(self.r[1]):int(self.r[1] + self.r[3]), int(self.r[0]):int(self.r[0] + self.r[2])]
         # cv2.imshow('roi', cropped)
