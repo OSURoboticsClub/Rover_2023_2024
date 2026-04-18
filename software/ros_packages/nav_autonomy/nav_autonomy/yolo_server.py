12aa67bfecb4e3ba03881a754fbf235918c59564
Henry Dalrymple <dalrymhe@oregonstate.edu>
2026-04-18 16:34:42 -0700

    fix none pose

--- software/ros_packages/nav_autonomy/nav_autonomy/yolo_server.py
+++ software/ros_packages/nav_autonomy/nav_autonomy/yolo_server.py
@@ -504,7 +504,7 @@ class YoloServer(Node):
                         feedback.center = center
                         feedback.top_left = top_left
                         feedback.bottom_right = bottom_right
-                        feedback.pose = None
+                        feedback.pose = PoseStamped()
 
                         if self.xc and self.yc and self.detected_camera_id is not None:
                             # Estimate depth from bounding box size
@@ -572,7 +572,6 @@ class YoloServer(Node):
                         feedback.confidence = 0.0
                         feedback.frame_id = frame_id
                         feedback.detected = False
-                        feedback.pose = None
                         goal_handle.publish_feedback(feedback)
 
                     # Display the frame (with or without detections)
