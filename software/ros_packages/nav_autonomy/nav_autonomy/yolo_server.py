63f30cb852cdb1e7a947796cb1812b38699eb09e
Kylan <kyrileyj@gmail.com>
2026-04-25 12:01:09 -0700

    fix yolo model path

--- software/ros_packages/nav_autonomy/nav_autonomy/yolo_server.py
+++ software/ros_packages/nav_autonomy/nav_autonomy/yolo_server.py
@@ -8,6 +8,9 @@ from rcl_interfaces.srv import GetParameters
 from rcl_interfaces.msg import ParameterType
 from visualization_msgs.msg import Marker
 from std_msgs.msg import Header
+from ament_index_python.packages import get_package_share_directory
+import os
+
 from nav_autonomy_interface.action import YoloFind
 from nav_autonomy_interface.srv import SwitchCamera
 
@@ -719,13 +722,13 @@ class YoloServer(Node):
 
         try:
             # Define model from action request
-            model = YOLO("yolo_models/mallet.pt")
+            model = YOLO(os.path.join(self.models_dir, "mallet.pt"))
             if goal_handle.request.search_object == YoloFind.Goal.BOTTLE:
-                model = YOLO("yolo_models/bottle.pt")
+                model = YOLO(os.path.join(self.models_dir, "bottle.pt"))
             elif goal_handle.request.search_object == YoloFind.Goal.ORANGE_HAMMER:
-                model = YOLO("yolo_models/mallet.pt")
+                model = YOLO(os.path.join(self.models_dir, "mallet.pt"))
             elif goal_handle.request.search_object == YoloFind.Goal.OG_HAMMER:
-                model = YOLO("yolo_models/hammer.pt")
+                model = YOLO(os.path.join(self.models_dir, "hammer.pt"))
             model.overrides['verbose'] = False
 
             cap = cv2.VideoCapture(self.source, cv2.CAP_V4L2)
