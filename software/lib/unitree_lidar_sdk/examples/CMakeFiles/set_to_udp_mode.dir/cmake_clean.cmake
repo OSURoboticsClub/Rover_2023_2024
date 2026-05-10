74c76a48ccd99f3fbb130e821c711cfa769134e2
Henry Dalrymple <dalrymhe@oregonstate.edu>
2026-05-09 21:09:32 -0700

    add unitree package from https://github.com/unitreerobotics/unilidar_sdk2

---
+++ software/lib/unitree_lidar_sdk/examples/CMakeFiles/set_to_udp_mode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/set_to_udp_mode"
+  "../bin/set_to_udp_mode.pdb"
+  "CMakeFiles/set_to_udp_mode.dir/set_to_udp_mode.cpp.o"
+  "CMakeFiles/set_to_udp_mode.dir/set_to_udp_mode.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/set_to_udp_mode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
