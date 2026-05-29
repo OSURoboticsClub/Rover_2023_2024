18b099746e98bcb19b2a94e3d20dfc5e5eed9639
Nolan Kessler <kesslnol@oregonstate.edu>
2026-05-29 06:46:36 -0700

    Gimbal control fixes

--- software/ros_packages/rover2_control/launch/gimbal_launch.py
+++ software/ros_packages/rover2_control/launch/gimbal_launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
             name = 'tower_gimbal',
             parameters = [
                 {"control_topic": "/tower_gimbal/control"},
-                {"can", "can_cam"},
+                {"can": "can_cam"},
                 {"direction": 1},
                 {"node_ids": [3, 5, 4]},
                 {"board_id": 2}
@@ -23,7 +23,7 @@ def generate_launch_description():
             name = 'chassis_gimbal',
             parameters = [
                 {"control_topic": "/chassis_gimbal/control"},
-                {"can", "can_cam"},
+                {"can": "can_cam"},
                 {"direction": 1},
                 {"node_ids": [1, -1, 0]},
                 {"board_id": 6}
