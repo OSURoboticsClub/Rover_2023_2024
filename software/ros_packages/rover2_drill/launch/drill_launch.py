d023f4791c77630464d84cb5b3865afba518bc43
Kylan <kyrileyj@gmail.com>
2026-05-29 15:34:00 -0700

    add drill ands spec launch files

---
+++ software/ros_packages/rover2_drill/launch/drill_launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    config = {
+        'emulate_tty': True,
+        'output': 'screen',
+        'respawn': True
+    }
+
+    return LaunchDescription([
+        Node(
+            package='rover2_drill',
+            executable='drill_cap_control',
+            name='drill_cap_control',
+            **config
+        ),
+        Node(
+            package='rover2_drill',
+            executable='drill_control',
+            name='drill_control',
+            **config
+        ),
+        Node(
+            package='rover2_drill',
+            executable='linear_actuator_control',
+            name='linear_actuator_control',
+            **config
+        )
+    ])
