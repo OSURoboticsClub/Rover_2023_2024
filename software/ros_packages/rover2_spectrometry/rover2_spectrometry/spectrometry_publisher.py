d1b9146c501ba7eb3669949108e3623c86a9f661
Aaron Goyal <aaron1.goyal@gmail.com>
2026-08-07 15:00:21 -0700

    changed camera settings for spectrometer

--- software/ros_packages/rover2_spectrometry/rover2_spectrometry/spectrometry_publisher.py
+++ software/ros_packages/rover2_spectrometry/rover2_spectrometry/spectrometry_publisher.py
@@ -111,7 +111,11 @@ class Spectrometry_Publisher(Node):
             plt.clf()
 
     def get_graph(self, feedNumber, reaction_type):
+
         cap = cv2.VideoCapture(self._camera_location + str(feedNumber))
+        cap.set(cv2.CAP_PROP_BRIGHTNESS, 60)
+        cap.set(cv2.CAP_PROP_CONTRAST, 110)
+        cap.set(cv2.CAP_PROP_SATURATION, 200)
         
         ret, frame = cap.read()
         cropped = frame[int(self.r[1]):int(self.r[1] + self.r[3]), int(self.r[0]):int(self.r[0] + self.r[2])]
